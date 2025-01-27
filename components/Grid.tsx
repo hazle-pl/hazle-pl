17b5945e80656ef27c04d55162930831dd497069
borzenx <139754558+hazle-pl@users.noreply.github.com>
2025-01-27 23:04:58 +0100

    Update Grid.tsx

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import useVisibilityAnimation from '@/lib/useVisibilityAnimation';
 
 interface ContainerProps {
   children: React.ReactNode;
@@ -23,17 +22,7 @@ const Grid: React.FC<ContainerProps> = ({ children, xs, md, xl, justify, style }
 
   return (
     <div className={`grid ${classes}`}>
-      {React.Children.map(children, (child) => {
-        const { isVisible, elementRef } = useVisibilityAnimation(0.3);
-        return (
-          <div
-            ref={elementRef}
-            className={`grid-item ${isVisible ? 'visible' : ''}`}
-          >
-            <div className="grid-item-content">{child}</div>
-          </div>
-        );
-      })}
+      {children}
     </div>
   );
 };
